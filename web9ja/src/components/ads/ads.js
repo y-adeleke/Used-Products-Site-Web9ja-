63911f596d429374a6fa28b7c9f0f436049de150
Yusuf O Adeleke <66679328+y-adeleke@users.noreply.github.com>
2023-12-13 16:51:25 -0500

    WEB9JA-46 I fixed this

--- web9ja/src/components/ads/ads.js
+++ web9ja/src/components/ads/ads.js
@@ -19,7 +19,7 @@ const Ads = () => {
 
   return (
     <main className={classes.mainBox}>
-      {adsContext?.ads?.map((ad, index) => {
+      {adsContext?.ads?.map((ad) => {
         const isActive = new Date(ad.endAt) >= new Date() && ad.isActive;
         const { category, condition, status } = adsContext.filterAdChoice;
         const matchesCategory = category === "all" || category === ad.category;
@@ -86,11 +86,3 @@ const Ads = () => {
   );
 };
 export default Ads;
-
-//is active --done
-//href email--done
-//clear text input afer comment --done
-//if you disable, dont see delete button. --done
-//set userprofile to default if no profile picture --done
-//only see active ads. --done
-//sign-in-signupthign --done
